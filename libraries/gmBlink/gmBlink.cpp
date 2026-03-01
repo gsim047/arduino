db7015f87028a8e5fdf588b9c8d1ccbda1406da8
user <gerasim@inbox.ru>
2026-03-01 21:58:40 +0300

    fix R61

--- libraries/gmBlink/gmBlink.cpp
+++ libraries/gmBlink/gmBlink.cpp
@@ -3,6 +3,34 @@
 
 
 
+// BUILTIN: 2/1
+// D0: 16/0
+// D1: 5/0
+// D2: 4/0
+// D5: 14/0
+// D6: 12/0
+// D7: 13/0
+
+
+int gmBlink::defN(const char *Pin)
+{
+	String pin(Pin);
+	if ( pin == "D0" )
+		return 16;
+	if ( pin == "D1" )
+		return 5;
+	if ( pin == "D2" )
+		return 4;
+	if ( pin == "D5" )
+		return 14;
+	if ( pin == "D6" )
+		return 12;
+	if ( pin == "D7" )
+		return 13;
+	return 2;
+}// gmBlink::defN
+
+
 bool gmBlink::defInv(int N)
 {
 	if ( N == 2 ) 
--- libraries/gmBlink/gmBlink.h
+++ libraries/gmBlink/gmBlink.h
@@ -1,12 +1,23 @@
 #ifndef __gmBlink_h
 #define __gmBlink_h 1
 
+// BUILTIN: 2/1
+// D0: 16/0
+// D1: 5/0
+// D2: 4/0
+// D5: 14/0
+// D6: 12/0
+// D7: 13/0
+
 
 class gmBlink {
 public:
 	gmBlink() : n(2), inv(true), ni(false) {}
-	gmBlink(int N) : n(N), inv(defInv(N)), ni(false) {}
+	gmBlink(int N) : n(N), inv(defInv(n)), ni(false) {}
 	gmBlink(int N, bool Inv) : n(N), inv(Inv), ni(false) {}
+	gmBlink(const char *pin) : n(defN(pin)), ni(false) {
+		inv = defInv(n);
+	}
 
 	gmBlink &operator=(const gmBlink &src){
 		if ( &src != this ){
@@ -17,6 +28,7 @@ public:
 		return *this;
 	}
 
+	static int defN(const char *pin);
 	static bool defInv(int N);
 
 	void bl(int tim) const;
