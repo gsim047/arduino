8bb0fb77d8ca81ca494e0fa7b9e232a576333855
user <gerasim@inbox.ru>
2026-03-01 22:11:12 +0300

    fix R61

--- Blink-ESP-01/Blink-ESP-01.ino
+++ Blink-ESP-01/Blink-ESP-01.ino
@@ -1,53 +1,29 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
+#include <Arduino.h>
 #include <gmBlink.h>
 
 
-gmBlink b;
+gmBlink b("BUILTIN");
 
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+void setup() 
+{
 //  pinMode(4, OUTPUT);
-  Serial.begin(115200);
-}
+	Serial.begin(115200);
+}// setup
 
-// the loop function runs over and over again forever
+
 void loop() 
 {
-	b.bl(100);
+	b.bl(50);
 	delay(500);
 //  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 //  delay(1000);                       // wait for a second
 //  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 //  delay(1000);                       // wait for a second
 
-    int sensorValue = analogRead(A0);
+	int sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
+	float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
-  Serial.println(voltage);
-}
+	Serial.println(voltage);
+}// loop
