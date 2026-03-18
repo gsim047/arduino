7d0fc49979a422d3a93698bbabffe8064f2c14e4
Georgy Stepanov <gsim047@gmail.com>
2026-03-19 01:39:08 +0300

    x230l

--- bin/ar_compile.sh
+++ bin/ar_compile.sh
@@ -5,4 +5,5 @@ MODEL=`cat /home/user/bin/esp_model.txt`
 echo arduino-cli compile ...
 echo model $MODEL
 #arduino-cli compile -b esp8266:esp8266:generic $1
-arduino-cli -v compile -e -b $MODEL $1
+#arduino-cli -v compile -e -b $MODEL $1
+arduino-cli compile -b $MODEL $1
