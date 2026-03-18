4689ccba1b8d726b860625539b266e79155ff6de
Georgy Stepanov <gsim047@gmail.com>
2026-03-18 15:48:21 +0300

    x230l

---
+++ gerkon/gerkon.ino
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+// геркон:
+//  нога 1 - на GND
+//  нога 2 - на D2
+//  нога 2 - через резистор 10 кОм на 5В
+
+const int pin = 4; // GPIO4 - это пин D2 на Wemos D1 Mini/NodeMCU
+static int state = LOW;
+
+
+void setup() 
+{
+	Serial.begin(115200);
+	// Используем встроенный резистор подтяжки к 3.3V
+	pinMode(pin, INPUT_PULLUP);
+}// setup
+
+
+void loop() 
+{
+	int rdState = digitalRead(pin);
+
+	if ( state != rdState ){
+		if ( rdState == LOW ){
+			Serial.println("closed (magnet is near)");
+		}else{
+			Serial.println("open (magnet is far)");
+		}
+		state = rdState;
+	}
+	delay(50);
+}// loop
