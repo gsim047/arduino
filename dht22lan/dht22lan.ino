e8aadcc3c2b0e6ea2cbba3a3d4c44a983344aa11
Georgy Stepanov <gsim047@gmail.com>
2026-03-28 19:14:59 +0300

    x230l

--- dht22lan/dht22lan.ino
+++ dht22lan/dht22lan.ino
@@ -3,9 +3,9 @@
 #include <DHT_U.h>
 //#include <DHT.h>
 
-//#include <gmUrl.h>
-#include <gmUrlS.h>
-#include "gsim047_ru.h"
+#include <gmUrl.h>
+//#include <gmUrlS.h>
+//#include "gsim047_ru.h"
 #include <gmCfgRead.h>
 
 
@@ -15,7 +15,8 @@
 
 //DHT dht(DHTPIN, DHTTYPE);
 
-gmUrlS url("http://gsim047.ru/esp/set.php");
+gmUrl url("http://192.168.1.201/esp/set.php");
+//gmUrlS url("https://gsim047.ru/esp/set.php");
 int n = 0;
 
 #define DHTPIN 2     // Digital pin connected to the DHT sensor 
@@ -49,33 +50,33 @@ void setup()
 		delay(1000);
 	}
 
-	url.fp = fingerprint_gsim047_ru;
+//	url.fp = fingerprint_gsim047_ru;
 	url.WiFi_connect();
 
-	dht.begin();
-	Serial.println(F("DHTxx Unified Sensor Example"));
-	// Print temperature sensor details.
-	sensor_t sensor;
-	dht.temperature().getSensor(&sensor);
-	Serial.println(F("------------------------------------"));
-	Serial.println(F("Temperature Sensor"));
-	Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-	Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-	Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-	Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-	Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-	Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-	Serial.println(F("------------------------------------"));
-	// Print humidity sensor details.
-	dht.humidity().getSensor(&sensor);
-	Serial.println(F("Humidity Sensor"));
-	Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-	Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-	Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-	Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-	Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-	Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-	Serial.println(F("------------------------------------"));
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
 }// setup
 
 
